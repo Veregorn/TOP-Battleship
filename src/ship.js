423717d89e5846e66db89fc156f21d2dba9b8483
Raul Jimenez <rjbarco@gmail.com>
2023-04-21 07:03:38 +0200

    Ship factory implemented

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,32 @@
 // Factory representing a Ship in the game
 const Ship = (name,length) => {
     
-    const _name = name; // Carrier 5 - Battleship 4 - Destroyer 3 - Submarine 3 - Patrol Boat 2
-    const _length = length; // Number of squares the ship occupies (see above)
-    let _hits = 0; // Number of times the ship has been damaged
-    let _sunk = false; // Indicates if the ship has been sunk or not
+    const _name = name // Carrier 5 - Battleship 4 - Destroyer 3 - Submarine 3 - Patrol Boat 2
+    const _length = length // Number of squares the ship occupies (see above)
+    let _hits = 0 // Number of times the ship has been damaged
+    let _sunk = false // Indicates if the ship has been sunk or not
+
+    const getName = () => _name
+
+    const getLength = () => _length
+
+    const hit = () => {
+        
+        _hits += 1
+
+        if (_hits === _length) {
+            _sunk = true
+        }
+    }
+
+    const isSunk = () => _sunk
+
+    return {
+        getName,
+        getLength,
+        hit,
+        isSunk
+    }
 }
+
+export default Ship
