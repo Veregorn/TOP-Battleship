095a3fb608bf4ec2dd245c471045d48bd02e28bd
Raul Jimenez <rjbarco@gmail.com>
2023-05-03 09:49:32 +0200

    Gameboard factory tests cover a lot of cases and everyone pass

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,9 +1,15 @@
 // Factory representing a game board in the game
 const Gameboard = () => {
 
-    const _board = [] // Array of 100 squares representing the game board
+    const _board = Array(100).fill("Water") // Array of 100 squares representing the game board
     const _ships = [] // Array of ships on the board
     let _gameover = false
+
+    // Get the board array
+    const getBoard = () => _board
+    
+    // Get the ships array
+    const getShips = () => _ships
 
     // Get if the game is over
     const getGameOver = () => _gameover
@@ -22,78 +28,101 @@ const Gameboard = () => {
     }
 
     // Set a ship in the array of ships
-    const setShip = (ship) => _ships.push(ship)
-    
-    // Creates the board
-    const createBoard = () => {
-        for (let i = 0; i < 100; i += 1) {
-            _board[i] = "Water";
-            setSquare(i,"Water")
-        }
-    }
+    const setShip = (ship) => getShips().push(ship)
+
+    // Return true if two squares are in the same line in the board
+    const isSameLine = (x,y) => Math.floor(x / 10) === Math.floor(y / 10)
+
+    // Return true if next square is in the same line or column that previous one
+    const isValidNextSquare = (current,next,direction) => 
+        direction === "x" ? isSameLine(next, current) : next <= 99
+
+    // Return true if a square is empty (no other ship is placed there)
+    const isEmptySquare = (square) => 
+        getSquare(square) === "Water"
+
+    // Gets next position in the board depending on ship direction placement
+    const getNextPosition = (currentPos,direction) => 
+        direction === "x" ? currentPos + 1 : currentPos + 10
 
     // Places a ship
     const placeShip = (ship,startPos,direction) => {
-        let i = 1
+        
         let nextPos = startPos
-        while (i <= ship.getLength()) {
+        const validPosArray = []
+
+        for (let i = 1; i <= ship.getLength(); i += 1) {
             
-            if (direction === "x") {
-                // Number has changed decade
-                if (Math.floor(nextPos / 10) !== Math.floor(startPos / 10)) {
-                    throw new Error("You are exceeding the limits of the board")
-                }
-            }
-            
-            if (direction === "y") {
-                if (nextPos > 99) {
-                    throw new Error("You are exceeding the limits of the board")
-                }
-            }
-            
-            // Set square string to ship name
-            setSquare(nextPos,ship.getName())
-            
-            if (direction === "x") {
-                nextPos += 1
-            }
-            
-            if (direction === "y") {
-                nextPos += 10
+            // Test if the next position is valid
+            if (!isValidNextSquare(startPos,nextPos,direction)) {
+                return { error: "You are exceeding the limits of the board" }
             }
 
-            i += 1
+            // Test if that square is empty (no other ship placed there)
+            if (!isEmptySquare(nextPos)) {
+                return { error: "That square is not empty" }
+            }
+            
+            // Insert the valid position into our temp array
+            validPosArray.push(nextPos)
+            
+            // Update position
+            nextPos = getNextPosition(nextPos,direction)
 
+        }
+
+        // Set square string to ship name for each value in the temp array
+        for (let i = 0; i < validPosArray.length; i += 1) {
+            setSquare(validPosArray[i],ship.getName())
         }
 
         // Place the ship in the array of ships
         setShip(ship)
+
+        // Return a success message
+        return { success: `A ${ship.getName()} has been placed` }
+
     }
 
     // Find and return a Ship in the board
     const findShip = (shipName) => {
-        for (let i = 0; i < _ships.length; i += 1) {
-            if (_ships[i].getName() === shipName) {
-                return _ships[i]
-            }
+        
+        const ship = getShips().find(s => s.getName() === shipName)
+        
+        // If no ship found, returns an error
+        if (!ship) {
+            return { error: "No ship found with that name" }
         }
-        // If no ship found, throw an error
-        throw new Error("No ship found with that name")
+
+        return ship
+
+    }
+
+    // Checks if the game is over
+    const checkGameOver = () => {
+        if (getShips().length === 0) {
+            setGameOver();
+        }
     }
 
     // Delete a ship from the ships array
     const deleteShip = (shipName) => {
-        for (let i = 0; i < _ships.length; i += 1) {
-            if (_ships[i].getName() === shipName) {
-                _ships.splice(i,1)
-                if (_ships.length === 0) {
-                    setGameOver()
-                }
-                return
-            }
+        
+        const index = getShips().findIndex(s => s.getName() === shipName)
+
+        // If no ship found, returns an error
+        if (index === -1) {
+            return { error: "There is no ship with that name to delete" }
         }
-        // If no ship found, throw an error
-        throw new Error("There is no ship with that name to delete")
+        
+        getShips().splice(index,1)
+                
+        // Check if the game is over
+        checkGameOver()
+
+        // Return a success message
+        return { success: `Ship named "${shipName}" has been deleted` }
+
     }
 
     // takes a pair of coordinates, determines whether or not the attack hit a ship and then 
@@ -101,49 +130,34 @@ const Gameboard = () => {
     // of the missed shot
     const receiveAttack = (x,y) => {
         
-        const squareNumber = (x * 10) + y
+        const squareNumber = x * 10 + y
+        const square = getSquare(squareNumber)
 
         // Attack fails
-        if (getSquare(squareNumber) === "Water") {
-            setSquare(squareNumber,"Hit")
+        if (square === "Water") {
+            setSquare(squareNumber,"Miss")
         } else { // Attack hits
-            const hitShip = findShip(getSquare(squareNumber))
-            setSquare(squareNumber,`${hitShip.getName()}-Hit`)
-            hitShip.hit()
+            const damagedShip = findShip(square)
+            setSquare(squareNumber,"ShipHit")
+            damagedShip.hit()
 
             // Need to test if ship is sunk
-            if (hitShip.isSunk()) {
-                deleteShip(hitShip.getName())
+            if (damagedShip.isSunk()) {
+                deleteShip(damagedShip.getName())
+                checkGameOver()
             }
         }
 
     }
 
-    // Prints board into the console
-    const printBoard = () => {
-        for (let i = 0; i < _board.length; i += 10) {
-            console.log(
-                getSquare(i).padEnd(15," "),
-                getSquare(i+1).padEnd(15," "),
-                getSquare(i+2).padEnd(15," "),
-                getSquare(i+3).padEnd(15," "),
-                getSquare(i+4).padEnd(15," "),
-                getSquare(i+5).padEnd(15," "),
-                getSquare(i+6).padEnd(15," "),
-                getSquare(i+7).padEnd(15," "),
-                getSquare(i+8).padEnd(15," "),
-                getSquare(i+9).padEnd(15," "),"\n")
-        }
-    }
-
     return {
         getGameOver,
         getSquare,
-        createBoard,
         placeShip,
         findShip,
         receiveAttack,
-        printBoard
+        getShips,
+        getBoard
     }
 
 }
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -6,14 +6,22 @@ import Ship from "../src/ship";
 let myBoard;
 
 beforeEach(() => {
-  myBoard = Gameboard();
-  myBoard.createBoard();
+  myBoard = Gameboard()
 });
 
-test("Board has been created but there is no ships on it. All the squares are 'Water'", () => {
-    for (let i = 0; i < 100; i += 1) {
-        expect(myBoard.getSquare(i)).toBe("Water")
-    }
+test("At start, board is empty of ships", () => {
+    const arr = myBoard.getBoard()
+    expect(arr.every(square => square === "Water")).toBeTruthy()
+})
+
+test("There are 100 squares in the board", () => {
+    const arr = myBoard.getBoard()
+    expect(arr.length).toBe(100)
+})
+
+test("At start, Ships array is empty", () => {
+    const shipsArr = myBoard.getShips()
+    expect(shipsArr.length).toBe(0)
 })
 
 test("A ship placed into the limits of the board is correctly placed (horizontally)", () => {
@@ -26,12 +34,15 @@ test("A ship placed into the limits of the board is correctly placed (horizontal
     }
     expect(myBoard.getSquare(68)).toBe("Water") // Next to (after)
     expect(myBoard.getSquare(62)).toBe("Water") // Next to (before)
+    const shipsArr = myBoard.getShips()
+    expect(shipsArr.length).toBe(1) // Length of ships array has changed
 })
 
 test("A ship placed into the limits of the board is correctly placed (vertically)", () => {
     const destroyer = Ship("Destroyer",3)
     let j = 70
-    myBoard.placeShip(destroyer,j,"y")
+    const result = myBoard.placeShip(destroyer,j,"y")
+    expect(result.success).toBe("A Destroyer has been placed")
     for (let i = 1; i <= destroyer.getLength(); i += 1) {
         expect(myBoard.getSquare(j)).toBe("Destroyer")
         j += 10
@@ -41,26 +52,32 @@ test("A ship placed into the limits of the board is correctly placed (vertically
 })
 
 test("Placing a ship out of the limits (y axis)", () => {
-    expect(() => myBoard.placeShip(Ship("Destroyer",3),80,"y"))
-        .toThrow(/^You are exceeding the limits of the board$/)
+    const result = myBoard.placeShip(Ship("Destroyer",3),80,"y")
+    expect(result.error).toBe("You are exceeding the limits of the board")
 })
 
 test("Placing a ship out of the limits (x axis)", () => {
-    expect(() => myBoard.placeShip(Ship("Destroyer",3),18,"x"))
-        .toThrow(/^You are exceeding the limits of the board$/)
+    const result = myBoard.placeShip(Ship("Destroyer",3),18,"x")
+    expect(result.error).toBe("You are exceeding the limits of the board")
 })
 
-test("Receives and attack and a ship is hit", () => {
+test("Checking ships array length after invalid placement", () => {
+    myBoard.placeShip(Ship("Destroyer",3),18,"x")
+    const ships = myBoard.getShips()
+    expect(ships.length).toBe(0)
+})
+
+test("Attack hits a ship and records the hit correctly", () => {
     const battleship = Ship("Battleship",4)
     myBoard.placeShip(battleship,74,"x")
     myBoard.receiveAttack(7,7)
     expect(battleship.getHits()).toBe(1)
-    expect(myBoard.getSquare(77)).toBe("Battleship-Hit")
+    expect(myBoard.getSquare(77)).toBe("ShipHit")
 })
 
-test("Receives and attack and no ship is hit", () => {
+test("Attack fails and records the miss correctly", () => {
     myBoard.receiveAttack(7,7)
-    expect(myBoard.getSquare(77)).toBe("Hit")
+    expect(myBoard.getSquare(77)).toBe("Miss")
 })
 
 test("All the ships have been sunk", () => {
@@ -71,4 +88,138 @@ test("All the ships have been sunk", () => {
     myBoard.receiveAttack(7,6)
     myBoard.receiveAttack(7,7)
     expect(myBoard.getGameOver()).toBeTruthy()
+    const ships = myBoard.getShips()
+    expect(ships.length).toBe(0)
+})
+
+// Multiple ships placement and interactions
+
+test("Two non-overlapping ships are placed correctly next to each other (horizontally)", () => {
+    const carrier = Ship("Carrier",5)
+    const submarine = Ship("Submarine",3)
+    myBoard.placeShip(carrier,10,"x")
+    myBoard.placeShip(submarine,15,"x")
+
+    for (let i = 10; i < 15; i += 1) {
+        expect(myBoard.getSquare(i)).toBe("Carrier")
+    }
+
+    for (let i = 15; i < 18; i += 1) {
+        expect(myBoard.getSquare(i)).toBe("Submarine")
+    }
+})
+
+test("Attack hits one ship and misses another", () => {
+    const carrier = Ship("Carrier",5)
+    const submarine = Ship("Submarine",3)
+    myBoard.placeShip(carrier,10,"x")
+    myBoard.placeShip(submarine,15,"x")
+    myBoard.receiveAttack(1,4)
+
+    // Control that hits the Carrier
+    expect(carrier.getHits()).toBe(1)
+
+    // Miss the Submarine
+    expect(submarine.getHits()).toBe(0)
+
+    // Attacked square is a hit
+    expect(myBoard.getSquare(14)).toBe("ShipHit");
+})
+
+test("One ship is sunk while another remains on the board", () => {
+    const carrier = Ship("Carrier",5)
+    const submarine = Ship("Submarine",3)
+    myBoard.placeShip(carrier,10,"x")
+    myBoard.placeShip(submarine,15,"x")
+
+    for (let i = 0; i < 5; i += 1) {
+        myBoard.receiveAttack(1,i)
+    }
+
+    // Carrier is sunk
+    expect(carrier.isSunk()).toBeTruthy()
+
+    // Submarine isn't sunk
+    expect(submarine.isSunk()).toBeFalsy()
+
+    // Game continues
+    expect(myBoard.getGameOver()).toBeFalsy()
+
+    // There is only one ship on the board
+    expect(myBoard.getShips().length).toBe(1)
+
+    // That ship is a submarine
+    expect(myBoard.getShips()[0].getName()).toBe("Submarine")
+})
+
+test("Two ships can't be placed on the same square", () => {
+    const carrier = Ship("Carrier",5)
+    const submarine = Ship("Submarine",3)
+    myBoard.placeShip(carrier,10,"x")
+    const result = myBoard.placeShip(submarine,14,"x")
+    
+    // Submarine placement results in an error
+    expect(result.error).toBe("That square is not empty")
+
+    // Submarine has not overlapped Carrier position
+    expect(myBoard.getSquare(14)).toBe("Carrier")
+
+    // Submarine has not been placed
+    expect(myBoard.getShips().length).toBe(1)
+})
+
+test("Two ships can't be placed on the same square (overlap happens in the last square of the second ship)", () => {
+    const carrier = Ship("Carrier",5)
+    const submarine = Ship("Submarine",3)
+    myBoard.placeShip(carrier,50,"x")
+    const result = myBoard.placeShip(submarine,33,"y")
+
+    // Submarine placement results in an error
+    expect(result.error).toBe("That square is not empty")
+
+    // Submarine has not been placed
+    expect(myBoard.getShips().length).toBe(1)
+
+    // First squares where we tried to place submarine are now "Water" ones
+    expect(myBoard.getSquare(33)).toBe("Water")
+    expect(myBoard.getSquare(43)).toBe("Water")
+
+    // Last square where we tried to place submarine is a "Carrier" one
+    expect(myBoard.getSquare(53)).toBe("Carrier")
+})
+
+// Other cases
+test("A ships is hit several times but isn't sunk", () => {
+    const battleship = Ship("Battleship",4)
+    myBoard.placeShip(battleship,74,"x")
+    myBoard.receiveAttack(7,4)
+    myBoard.receiveAttack(7,5)
+    myBoard.receiveAttack(7,6)
+    
+    // Ship isn't sunk
+    expect(battleship.isSunk()).toBeFalsy()
+
+    // Game is still in course
+    expect(myBoard.getGameOver()).toBeFalsy()
+    
+    // There is one ship in the game
+    const ships = myBoard.getShips()
+    expect(ships.length).toBe(1)
+})
+
+// Edge cases
+test("A ship can be placed in a corner", () => {
+    const battleship = Ship("Battleship",4)
+    myBoard.placeShip(battleship,96,"x")
+
+    // Ship has been placed correctly
+    for (let i = 96; i < 100; i += 1) {
+        expect(myBoard.getSquare(i)).toBe("Battleship")
+    }
+
+    // There is one ship in the board
+    expect(myBoard.getShips().length).toBe(1)
+
+    // The ship is a Battleship
+    expect(myBoard.getShips()[0].getName()).toBe("Battleship")
 })
