f881a98d4f1df1e42c7f152a2b3d6c15cd45e7a1
Raul Jimenez <rjbarco@gmail.com>
2023-05-04 06:46:57 +0200

    'generateAttack' method implemented in factory 'Player' to randomly attack enemy gameboard

--- src/player.js
+++ src/player.js
@@ -6,6 +6,7 @@ const Player = (type,ships) => {
     const _gameBoard = Gameboard() // Each player has a game board
     const _type = type // Possible values: "Human" or "AI"
     const _ships = ships // Array of ships a player is provided with
+    const _availableAttacks = Array.from({length: 100}, (_, index) => index) // Creates an array from 0 to 99
 
     // Gets the game board
     const getGameBoard = () => _gameBoard
@@ -18,11 +19,11 @@ const Player = (type,ships) => {
 
     // Gets ship at position in the array of player's ships
     const getShipAtPos = (pos) => _ships[pos]
+
+    // Gets the attacks array
+    const getAvailableAttacks = () => _availableAttacks
     
     const placeShips = () => {
-
-        // We need to fill the game board squares
-        getGameBoard().createBoard()
 
         if (getPlayerType() === "AI") {
             
@@ -33,16 +34,19 @@ const Player = (type,ships) => {
                 let success = false
 
                 while (!success) {
-                    try {
-                        // Square and orientation are random generated
-                        getGameBoard().
-                            placeShip(
-                                getShipAtPos(i),
-                                Math.floor(Math.random() * 100),
-                                Math.random() < 0.5 ? "x" : "y")
+                    // Square and orientation are random generated
+                    const result = getGameBoard().
+                        placeShip(
+                            getShipAtPos(i),
+                            Math.floor(Math.random() * 100),
+                            Math.random() < 0.5 ? "x" : "y")
+                    
+                    // Test if ship was placed successfully
+                    if (result.success) {
                         success = true
-                    } catch (error) {
-                        console.error(`Error: ${error.message}`)
+                        console.log(`Result: ${result.success}`)
+                    } else {
+                        console.error(`Error: ${result.error}`)
                     }
                 }                
             }
@@ -50,8 +54,30 @@ const Player = (type,ships) => {
         }
     }
 
+    // Generates a random index from that array of available attacks
+    const generateRandomIndex = () => 
+        Math.floor(Math.random() * getAvailableAttacks().length)
+
+    // When we attack a position in enemy's board 
+    // we need to delete it from available attacks to not repeat it in the future
+    const deleteFromAvailableAttacks = (index) => {
+        _availableAttacks.splice(index,1)
+    }
+
+    // Generates a square to attack
+    const generateAttack = () => {
+            
+            const index = generateRandomIndex()
+            const square = getAvailableAttacks[index]
+            deleteFromAvailableAttacks(index)
+            return square
+
+    }
+
     return {
-        placeShips
+        getGameBoard,
+        placeShips,
+        generateAttack
     }
 
 }
