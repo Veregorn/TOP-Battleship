0eb64fe3619e29039a8441c377c78e66fbbf9332
Raul Jimenez <rjbarco@gmail.com>
2023-05-08 09:24:30 +0200

    getRandomDirection and manualAttack methods implemented in gameboard.js. New tests implemented in player.test.js

--- src/player.js
+++ src/player.js
@@ -26,35 +26,47 @@ const Player = (type,ships) => {
     // Gets the square at 'pos' in the '_availableAttacks' array
     const getAttackAtPos = (pos) => _availableAttacks[pos]
 
+    // Gets index of a square in the '_availableAttacks' array
+    const getIndexOfAttack = (square) => _availableAttacks.indexOf(square)
+
     // Receives a square in returns true if that square hasn't been attacked yet
     const isValidAttack = (square) => getAvailableAttacks().includes(square)
+
+    // Gets a random value "x" or "y" for the orientation of a ship
+    const getRandomDirection = () => (Math.random() < 0.5 ? "x" : "y")
+
+    // Randomly shuffles an array 
+    const shuffleArray = (array) => {
+        const shuffledArray = [...array];
+        for (let i = shuffledArray.length - 1; i > 0; i -= 1) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+        }
+        return shuffledArray
+    }
     
     const placeShips = () => {
 
         if (getPlayerType() === "AI") {
             
-            // Place every ship player has
+            // Creates an ordered array 0 to 99
+            const startPositionCandidates = Array.from({ length: 100 }, (_, index) => index);
+            // Shuffle array positions            
+            const shuffledPositions = shuffleArray(startPositionCandidates);
+
+            // For each ship this player has
             for (let i = 0; i < getShips().length; i += 1) {
                 
-                // Keep in while loop until success
-                let success = false
+                // Iterate "shuffledPositions" array until find a valid ship placement
+                for (let j = 0; j < shuffledPositions.length; j += 1) {
+                    const direction = getRandomDirection();
+                    const result = getGameBoard().placeShip(getShipAtPos(i), shuffledPositions[j], direction);
 
-                while (!success) {
-                    // Square and orientation are random generated
-                    const result = getGameBoard().
-                        placeShip(
-                            getShipAtPos(i),
-                            Math.floor(Math.random() * 100),
-                            Math.random() < 0.5 ? "x" : "y")
-                    
-                    // Test if ship was placed successfully
                     if (result.success) {
-                        success = true
-                        console.log(`Result: ${result.success}`)
-                    } else {
-                        console.error(`Error: ${result.error}`)
+                        console.log(`Result: ${result.success}`);
+                        break;
                     }
-                }                
+                }
             }
 
         }
@@ -71,7 +83,7 @@ const Player = (type,ships) => {
     }
 
     // Generates a square to attack
-    const generateAttack = () => {
+    const generateAutoAttack = () => {
             
             const index = generateRandomIndex()
             const square = getAttackAtPos(index)
@@ -80,11 +92,30 @@ const Player = (type,ships) => {
 
     }
 
+    // Human player attacks an specific location
+    const manualAttack = (square) => {
+
+        // Test if is valid attack
+        if (!isValidAttack(square)) {
+            return { error: "Invalid attack!" }
+        }
+
+        // Delete tha square from valid attacks array
+        deleteFromAvailableAttacks(getIndexOfAttack(square))
+
+        // Return success msg
+        return {success: "Position attacked!"}
+    }
+
     return {
         getGameBoard,
         placeShips,
-        generateAttack,
-        isValidAttack
+        generateAutoAttack,
+        isValidAttack,
+        manualAttack,
+        getPlayerType,
+        getShips,
+        getRandomDirection
     }
 
 }
